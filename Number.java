53bd7d9ff561743482bcdaa3b384851c0437300a
geode3 <132504158+geode3@users.noreply.github.com>
2025-02-28 23:52:53 -0600

    Update Number.java

--- Number.java
+++ Number.java
@@ -1,35 +1,54 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
+//Name - Giovani Zuniga
 
-public class Number
-{
-	private int theValue;
-	
-	public Number(int value)
-	{
+public class Number {
+    private int theValue; // The integer value stored in this object
 
+    /**
+     * Constructs a Number object with the specified value
+     * @param value The integer value to be stored
+     */
+    public Number(int value) {
+        theValue = value;
+    }
 
-	}	
-	
-	public int getValue()
-	{
-		return theValue;
-	}
-	
-	public boolean equals(Object obj)
-	{
-		return false;
-	} 
-	
-	public int hashCode()
-	{
-		return 0;
-	}
+    /**
+     * gets the integer value stored in this object.
+     * @return The stored integer value
+     */
+    public int getValue() {
+        return theValue;
+    }
 
-	public String toString()
-	{
-		return "";
-	}	
+    /**
+     * Compares this Number object with another for equality
+     * @param obj The object to compare with
+     * @return true if the objects store the same integer value, false otherwise
+     */
+    public boolean equals(Object obj) {
+        Number other = (Number) obj;
+        if(this.theValue == other.theValue){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * Gets a hash code for this Number object
+     * @return The remainder when the integer value is divided by 10
+     */
+    public int hashCode() {
+        return theValue % 10;
+    }
+
+    /**
+     * Returns a string representation of the stored integer value
+     * @return A string representation of the integer
+     */
+    public String toString() {
+        return Integer.toString(theValue);
+    }
 }
